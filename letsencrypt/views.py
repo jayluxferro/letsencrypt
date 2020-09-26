f36c5332ada96152dbcd5bc339b24c0287f0f822
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-09-26 08:29:48 +0000

    bug fix

--- letsencrypt/views.py
+++ letsencrypt/views.py
@@ -6,5 +6,5 @@ def lets_encrypt(request, cid):
     try:
         ch_default = os.environ['LE_CHALLENGES']
     except:
-        ch_default = ".-nKGkamsfmmJUr8dFAFKOPtuyAedMginKBLHr-LyX0s"
-    return HttpResponse(cid + ch_default, content_type="text/plain")
+        ch_default = "-nKGkamsfmmJUr8dFAFKOPtuyAedMginKBLHr-LyX0s"
+    return HttpResponse(cid + "." + ch_default, content_type="text/plain")
